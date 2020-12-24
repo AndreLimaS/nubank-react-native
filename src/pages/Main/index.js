22ee006673dd7107c777acd6f256c6d894394a9a
andre <a.s.l0@hotmail.com>
2020-12-24 16:01:06 -0300

    feat: created card main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,44 @@
 import React from "react";
+import Icon from "react-native-vector-icons/MaterialIcons";
+
 import Header from "../../components/Header/index";
 import Tabs from "../../components/Tabs";
 
-import { Container } from "./styles";
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Descriptions,
+  Annotation,
+} from "./styles";
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponivel</Title>
+            <Descriptions>R$ 200.354,55</Descriptions>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferencia de R$ 20,00 recebida de Andre Lima as 08:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
