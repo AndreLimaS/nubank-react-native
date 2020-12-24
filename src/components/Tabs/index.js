044db64c84e33f4a2752181680daffd7cdcdeb3e
andre <a.s.l0@hotmail.com>
2020-12-24 15:20:11 -0300

    feat: created component Tabs struct initial

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-import { Container } from "./styles";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { Container, TabsContainer, TabItem, TabText } from "./styles";
 
 export default function Tabs() {
-  return <Container />;
+  return (
+    <Container>
+      <TabsContainer>
+        <TabItem>
+          <Icon name="person-add" size={24} color="#FFF" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+      </TabsContainer>
+    </Container>
+  );
 }
